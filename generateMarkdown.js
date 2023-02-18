735b3acb56a2d16f5f00308b88988ecb19ce2a25
bex-ford <becky782@hotmail.co.uk>
2023-02-18 11:50:13 +0000

    added video to README.md

---
+++ generateMarkdown.js
@@ -0,0 +1,33 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.project}
+[![License:${data.license}](https://img.shields.io/badge/License-${data.license}-yellow.svg)](https://opensource.org/licenses/${data.license})
+## Description:
+${data.description}
+## Table of Contents:
+* [Installation](#installation)
+* [Usage](#usage)
+* [Licence](#licence)
+* [Contributing](#contributing)
+* [Tests](#tests)
+### Installation:
+To install the necessary dependencies, open the console and run the following:
+\`\`\`${data.installation}\`\`\`
+### Usage:
+${data.usage}
+### Licence:
+This project is licensed under:
+${data.license}
+### Contributing:
+${data.contribute}
+### Tests:
+To test open the console and run the following:
+\`\`\`${data.tests}\`\`\`
+### Contact details:
+If you have any questions please contact via email ${data.email} or 
+[${data.github}](https://github.com/${data.github})
+`;
+}
+
+module.exports = generateMarkdown;
+ 
---
+++ generatedREADME.md
@@ -0,0 +1,26 @@
+# module-11
+[![License:MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description:
+i will use this code for future projects
+## Table of Contents:
+* [Installation](#installation)
+* [Usage](#usage)
+* [Licence](#licence)
+* [Contributing](#contributing)
+* [Tests](#tests)
+### Installation:
+To install the necessary dependencies, open the console and run the following:
+```inquirer and node```
+### Usage:
+n/a
+### Licence:
+This project is licensed under:
+MIT
+### Contributing:
+my motivation for this project was to create a template README file using node
+### Tests:
+To test open the console and run the following:
+```npm test```
+### Contact details:
+If you have any questions please contact via email becky782@hotmail.co.uk or 
+[bex-ford](https://github.com/bex-ford)
